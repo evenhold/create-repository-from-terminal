7262e092b5df0b8a61bd7cce85d43ae1e9906bfb
Franz Victorio <evenhold@gmail.com>
2017-05-15 00:00:19 -0500

    update

---
+++ create-repo.sh
@@ -0,0 +1,5 @@
+git init
+curl -u $(user) https://api.github.com/user/repos -d '{"name":$(repo),"description":$(descrip)}'
+# Remember replace USER with your username and REPO with your repository/application name!
+git remote add origin git@github.com/$(user)/$(repo).git
+git push origin master
